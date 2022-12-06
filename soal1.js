03b96b44c37bbfcb219a51cc791093e442b3aa5b
ghifar1 <muhamadalghifari97@gmail.com>
2022-12-06 17:29:30 +0700

    change format currency

--- soal1.js
+++ soal1.js
@@ -1,6 +1,6 @@
 const reader = require('./consoleReader')
 
-const formatter = new Intl.NumberFormat('en-US', {
+const formatter = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
     maximumFractionDigits: 0,
